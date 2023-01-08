97946df62085a50ff4a85eaecb47d3f74a6e6808
EddoDeuz <edgardojnx@outlook.com>
2023-01-08 17:01:43 -0300

    para tp9

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.get("/", (req, res) =>
 app.get("/ayuda", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "ayuda.html"))
 );
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
 app.get("/carrito", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "carrito.html"))
 );
